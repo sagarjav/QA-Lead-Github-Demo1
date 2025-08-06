1266a20ad419661865c3a8162f3389a181bdb356
sagar-bankar <sagar.bankar590@gmail.com>
2025-08-06 21:18:16 +0530

    added additional checks--->sagar tester Branchx--->QA Lead suggestion

--- src/tests/Test2.java
+++ src/tests/Test2.java
@@ -5,6 +5,8 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Sagar Tester added this code");
+		//after review PR by QA lead chnages tester made
+		System.out.println("Sagar Tester Added check point as per QA Lead suggestions");
 	}
 
 }
