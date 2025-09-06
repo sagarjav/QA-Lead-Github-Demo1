e79d29fd861786919c955b8eead454fa65c79b56
sagar-bankar <sagar.bankar590@gmail.com>
2025-09-06 13:32:01 +0530

    sagar tester---->6 sep test cases added

--- src/tests/Test2.java
+++ src/tests/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Sagar Tester added this code");
+		System.out.println("Sagar Tester added this This test ");
 		//after review PR by QA lead chnages tester made
 		System.out.println("Sagar Tester Added check point as per QA Lead suggestions");
 	}
